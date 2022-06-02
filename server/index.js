2718c6590c0655a797285bae37f7e7f85e3783fb
sagar-aryal <sagar_yal92@outlook.com>
2022-06-02 23:17:31 +0300

    connected to mongoose successfully

--- server/index.js
+++ server/index.js
@@ -1,7 +1,32 @@
 import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
 
-app.listen(5000, () => {
-  console.log("Server has started at port 5000!");
+dotenv.config();
+
+app.use(express.json());
+
+const port = process.env.PORT || 8000;
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(console.log("Mongoose is connected to database"))
+  .catch((err) => console.log(err.message));
+
+app.listen(port, () => {
+  console.log(`Server has started at ${port}.`);
 });
